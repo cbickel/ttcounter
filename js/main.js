f47751e1f184a5be4e218460ef2546f1836d0977
Christian Bickel <cbickel@de.ibm.com>
2016-11-01 01:08:28 +0100

    Let browser listen on changes from IOT

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
-// Insert names from Query-Parameters
 $(document).ready(function(){
+    // Insert names from Query-Parameters
     var queryParameters = window.location.search.substring(1).split("&");
-
     queryParameters.forEach(function(parameter) {
       var keyValue = parameter.split("=");
       switch(keyValue[0]) {
@@ -12,6 +11,30 @@ $(document).ready(function(){
           $("#namePlayer2").text(keyValue[1]);
           break;
       }
+    });
+
+    //IotfDevice
+    var config = {
+      "org" : "gm7xvu",
+      "type" : "Browser",
+      "id" : "browser",
+      "auth-key" : "a-gm7xvu-unqt2bj20h",
+      "auth-token" : "8oMm9wLB0vyWNjtuH!"
+    };
+    var appClient = new IBMIoTF.IotfApplication(config);
+    appClient.connect();
+    appClient.log.setLevel('trace');
+    appClient.on('connect', function () {
+      appClient.subscribeToDeviceEvents("Whisk-Action","whisk","score");
+    });
+    appClient.on("deviceEvent", function (deviceType, deviceId, eventType, format, payload) {
+      console.log("Device Event from :: " + deviceType + " : " + deviceId + " of event " + eventType + " with payload : " + payload);
+      var pay = JSON.parse(payload);
+
+      $("#scoreP1").text(pay.p1);
+      $("#scoreP2").text(pay.p2);
+      $("#setP1").text(pay.p1set);
+      $("#setP2").text(pay.p2set);
     });
   }
 );
